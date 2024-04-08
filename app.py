ccacad03adeeb87630e910e8501acbcbf6ad39c6
shuguruiua <popov.yk@gmail.com>
2024-04-08 07:23:53 +0300

    First commit

--- app.py
+++ app.py
@@ -1,17 +1,56 @@
 from http.client import HTTPException
+from pathlib import Path
 
-from fastapi import FastAPI, Depends
+import redis.asyncio as redis
+from fastapi import FastAPI, Depends, Request
+from fastapi_limiter import FastAPILimiter
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.db import get_db
-from src.routes import contacts
-from src.routes import auth
+from src.routes import contacts, auth, users
+from src.conf.config import config
 
 app = FastAPI()
 
+origins = ["http://localhost:8000",
+           "http://127.0.0.1:8000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router, prefix='/api')
+app.include_router(users.router, prefix='/api')
 app.include_router(contacts.router, prefix='/api')
+
+BASE_DIR = Path(".")
+
+app.mount("/static", StaticFiles(directory=BASE_DIR / "src" / "static"), name="static")
+
+
+@app.on_event("startup")
+async def startup():
+    r = await redis.Redis(host=config.REDIS_DOMAIN, port=config.REDIS_PORT, password=config.REDIS_PASSWORD, db=0)
+    await FastAPILimiter.init(r)
+
+
+templates = Jinja2Templates(directory=BASE_DIR / "src" / "templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "our": "Contacts_web 1.0"}
+    )
 
 
 @app.get('/')
